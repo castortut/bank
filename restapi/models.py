324fba154cfbf74a8f3be395e81617a116306ac8
Esa Varemo <esa@kuivanto.fi>
2017-04-08 16:58:24 +0300

    Shaping database models

--- restapi/models.py
+++ restapi/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+import bcrypt
+
+
+class Account(models.Model):
+    """
+    Generally each user holds one account
+    """
+
+    creation_date = models.DateTimeField()
+    enabled = models.BooleanField(default=True)
+
+    name = models.CharField(max_length=64)
+    passwordhash = models.CharField(max_length=64)
+
+    # Balance in cents
+    balance = models.IntegerField(default=0)
+
+    def set_password(self, password):
+        self.passwordhash = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    def verify_password(self, password):
+        return password == bcrypt.checkpw(password, self.passwordhash)
+
+
+class Token(models.Model):
+    """
+    Token that is used to identify the account. Can be multiple
+    """
+    creation_date = models.DateTimeField()
+
+    serialhash = models.CharField(max_length=64)
+    account = models.ForeignKey(Account)
+
+
+class Transaction(models.Model):
+    """
+    Record happened transactions
+    """
+    date = models.DateTimeField()
+
+    amount = models.IntegerField()
+    account = models.ForeignKey(Account)

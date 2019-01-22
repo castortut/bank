ef54a16450b7f48f49e63b7cc745bdeb7618dd4d
Esa Varemo <esa@kuivanto.fi>
2019-01-22 22:09:01 +0200

    Make the postgres database into an env variable

--- bank/settings.py
+++ bank/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'bank.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kubernetes_django',
+        'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
